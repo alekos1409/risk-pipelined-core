d9cf3c92f5b1f63dd67e20e512a9c21bd06d4ad7
alekos1409 <alekoskipouros448@gmail.com>
2026-06-30 17:02:39 +0300

    Create instr_mem_test.v

---
+++ testbenches/instr_mem_test.v
@@ -0,0 +1,30 @@
+module instr_mem_test();
+reg [31:0]addr;
+wire [31:0]instruction;
+instr_mem uut(
+ .addr(addr),
+ .instruction(instruction)
+    
+);
+initial begin 
+    addr = 32'h00000000;
+    #1;
+    $display("instruction = %b", instruction);
+    
+    addr = 32'h00000004;
+    #1;
+    $display("instruction = %b", instruction);
+    
+    addr = 32'h00000008;
+    #1;
+    $display("instruction = %b", instruction);
+    
+    addr = 32'h0000001C;  // αυτό είναι 28 σε hex - μπορείς να επιβεβαιώσεις;
+    #1;
+    $display("instruction = %b", instruction);
+    
+    $finish;
+
+end
+
+endmodule

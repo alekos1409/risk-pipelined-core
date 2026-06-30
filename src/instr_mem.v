a2e9fc340a67d86457a664c6ef734124d5f947c2
alekos1409 <alekoskipouros448@gmail.com>
2026-06-30 17:02:44 +0300

    Create instr_mem.v

---
+++ src/instr_mem.v
@@ -0,0 +1,7 @@
+module instr_mem(addr,instruction);
+input [31:0]addr;
+output [31:0]instruction;
+reg [31:0] mem [1023:0];
+initial $readmemh("src/program.hex", mem);
+assign instruction = mem[addr[11:2]];
+endmodule

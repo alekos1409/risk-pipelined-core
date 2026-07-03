1df7c533cdf3dad031dc4e0a8b3951622788db1c
alekos1409 <alekoskipouros448@gmail.com>
2026-07-03 10:57:12 +0300

    Update alu_control.v

--- src/alu_control.v
+++ src/alu_control.v
@@ -1,4 +1,4 @@
-module alu_control(instruction,ALUOp,ALUcontrol,func7);
+module alu_control(instruction,ALUOp,ALUcontrol);
 input [31:0]instruction;
 input [1:0]ALUOp;
 wire [2:0]func3;
@@ -11,19 +11,17 @@ always @(*)begin
   case(ALUOp)
 2'b00:begin ALUcontrol = 3'b000;end
 2'b01:begin ALUcontrol = 3'b001;end
-2'b10:begin if(func3 == 3'b000)
-case(func7_5)
-1'b0:ALUcontrol = 3'b000;
-1'b1:ALUcontrol = 3'b001;
-endcase
-else
-case(func3)
-3'b110:ALUcontrol = 3'b011;
-3'b111:ALUcontrol = 3'b010;
-3'b010:ALUcontrol = 3'b101;
-endcase
+2'b10: begin
+    if(func3 == 3'b000) begin
+        if(funct7_5 == 1'b0)
+            ALUcontrol = 3'b000;  
+        else
+            ALUcontrol = 3'b001;  
+    end
+    else if(func3 == 3'b110) ALUcontrol = 3'b011; 
+    else if(func3 == 3'b111) ALUcontrol = 3'b010; 
+    else if(func3 == 3'b010) ALUcontrol = 3'b101; 
 end
   endcase
-    end
-
+end
 endmodule

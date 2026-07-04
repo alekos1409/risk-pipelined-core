bbe4c6c553868284275c68e1a96a60e7f3a6742f
alekos1409 <alekoskipouros448@gmail.com>
2026-07-04 16:47:13 +0300

    Create write_back.v

---
+++ src/write_back.v
@@ -0,0 +1,8 @@
+module write_back(MemToRegW,ReadDataW,ALuResultW,ResultW);
+input clk,reset,MemToRegW;
+input [31:0]ReadDataW,ALuResultW;
+output [31:0]ResultW;
+assign ResultW = MemToRegW ? ReadDataW : ALuResultW;
+endmodule
+
+

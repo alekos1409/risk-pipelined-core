67606fa48b0ba458ae311f2e01464e6020f85be9
alekos1409 <alekoskipouros448@gmail.com>
2026-07-06 15:19:43 +0300

    Update data_memory.v

--- src/data_memory.v
+++ src/data_memory.v
@@ -3,6 +3,12 @@ input clk,we;
 input [31:0]address,data_in;
 output [31:0]data_out;
 reg [31:0] regs [1023:0];
+integer i;
+
+initial begin
+    for(i=0;i<1024;i=i+1)
+        regs[i]=32'b0;
+end
 always @(posedge clk) 
 if (we==1 ) begin regs[address[11:2]]<=data_in;
 end

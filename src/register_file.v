9af9110622fa4e6ab85e101556214f922d8fdaf5
alekos1409 <alekoskipouros448@gmail.com>
2026-07-06 15:19:30 +0300

    Update register_file.v

--- src/register_file.v
+++ src/register_file.v
@@ -1,19 +1,30 @@
-module register_file(we,rs1_addr,rs2_addr,clk,rd_addr,rs1_data,rs2_data,rd_data);
-input [4:0]rs1_addr,rs2_addr,rd_addr;
-output [31:0]rs1_data,rs2_data;
+module register_file(
+    input we,
+    input clk,
+    input [4:0] rs1_addr, rs2_addr, rd_addr,
+    input [31:0] rd_data,
+    output [31:0] rs1_data, rs2_data
+);
+
 reg [31:0] regs [0:31];
-input clk,we;
-input [31:0] rd_data;
-assign rs1_data = regs[rs1_addr];
-assign rs2_data = regs[rs2_addr];
 integer i;
+
 initial begin
-    for (i = 0; i < 32; i = i + 1)
+    for(i = 0; i < 32; i = i + 1)
         regs[i] = 32'b0;
 end
+
+// Write port
 always @(posedge clk) begin
-    if(we && rd_addr != 5'b0)begin
-        regs[rd_addr]<=rd_data;
+    if (we && rd_addr != 5'd0)
+        regs[rd_addr] <= rd_data;
 end
-end
+
+// Read bypass
+assign rs1_data = (we && (rd_addr == rs1_addr) && (rd_addr != 5'd0))
+                ? rd_data : regs[rs1_addr];
+
+assign rs2_data = (we && (rd_addr == rs2_addr) && (rd_addr != 5'd0))
+                ? rd_data : regs[rs2_addr];
+
 endmodule

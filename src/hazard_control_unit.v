06c75c734e4cf7c85eaffb5b3aa44da80113f2fe
alekos1409 <alekoskipouros448@gmail.com>
2026-07-06 15:19:37 +0300

    Create hazard_control_unit.v

---
+++ src/hazard_control_unit.v
@@ -0,0 +1,21 @@
+module hazard_control_unit(PCSrcE,RdE,Rs1D,Rs2D,MemReadE,flushD,flushE,stallD,stallF);
+input PCSrcE,MemReadE;
+input [4:0] Rs1D,Rs2D,RdE;
+output flushD,flushE,stallD,stallF;
+reg LoadUseHazard;
+always @(*)begin
+if (MemReadE) begin
+if ((RdE == Rs1D | RdE == Rs2D) & RdE != 0 ) begin
+ LoadUseHazard = 1'b1;
+end
+else
+ LoadUseHazard = 1'b0;
+end
+else
+LoadUseHazard = 1'b0;
+end
+assign flushD = PCSrcE;
+assign flushE = PCSrcE | LoadUseHazard;
+assign stallD = LoadUseHazard;
+assign stallF= LoadUseHazard;
+endmodule 

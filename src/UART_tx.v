e834cbe36f8a7798451634baa85481fa09b77350
alekos1409 <alekoskipouros448@gmail.com>
2026-07-10 09:43:44 +0300

    Create UART_tx.v

---
+++ src/UART_tx.v
@@ -0,0 +1,68 @@
+module UART_tx(clk,MemWriteM_UART,reset,data_in,tx,busy);
+input clk,MemWriteM_UART,reset;
+input [7:0] data_in;
+output reg tx,busy;
+reg [7:0] shift_reg;
+reg[2:0]bit_counter;
+parameter idle = 2'b0, start = 2'b01, data = 2'b10 , stop = 2'b11; 
+parameter CLKS_PER_BIT = 4;
+reg [7:0] cycle_counter;
+reg [1:0] state = idle;
+always @(posedge clk)begin
+if (reset)begin
+    cycle_counter <= 0;
+    tx <= 1;
+    busy <= 0;
+    state <= idle;
+shift_reg <= 0;
+bit_counter <= 0;
+end
+else begin
+case(state)
+idle : begin
+    tx <= 1;
+    busy <= 0;
+    if(MemWriteM_UART)begin
+    state <= start;
+    shift_reg <= data_in;
+    end
+    else state <= idle;
+end
+start : begin
+ tx<=0;
+  busy <= 1;
+ if (cycle_counter == CLKS_PER_BIT - 1) begin
+        cycle_counter <= 0;
+        state <= data;
+    end
+    else
+        cycle_counter <= cycle_counter + 1;
+end
+data: begin
+    tx <= shift_reg[bit_counter];
+     busy <= 1;
+    if (cycle_counter == CLKS_PER_BIT - 1) begin
+        cycle_counter <= 0;
+        if (bit_counter == 3'h7)
+            state <= stop;
+        else
+            bit_counter <= bit_counter + 1;
+    end
+    else
+        cycle_counter <= cycle_counter + 1;
+end
+stop: begin
+    tx <= 1;
+    busy <= 1;
+    if (cycle_counter == CLKS_PER_BIT - 1) begin
+        cycle_counter <= 0;
+        state <= idle;
+        bit_counter <= 0;
+    end
+    else
+        cycle_counter <= cycle_counter + 1;
+end
+endcase
+end
+end
+endmodule

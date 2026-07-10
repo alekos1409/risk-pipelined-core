be2524e10002d664f20c44dca5f967db21d78925
alekos1409 <alekoskipouros448@gmail.com>
2026-07-10 09:43:49 +0300

    Update register_file.v

--- src/register_file.v
+++ src/register_file.v
@@ -5,24 +5,18 @@ module register_file(
     input [31:0] rd_data,
     output [31:0] rs1_data, rs2_data
 );
-
 reg [31:0] regs [0:31];
 integer i;
-
 initial begin
     for(i = 0; i < 32; i = i + 1)
         regs[i] = 32'b0;
 end
-
 always @(posedge clk) begin
     if (we && rd_addr != 5'd0)
         regs[rd_addr] <= rd_data;
 end
-
 assign rs1_data = (we && (rd_addr == rs1_addr) && (rd_addr != 5'd0))
                 ? rd_data : regs[rs1_addr];
-
 assign rs2_data = (we && (rd_addr == rs2_addr) && (rd_addr != 5'd0))
                 ? rd_data : regs[rs2_addr];
-
 endmodule

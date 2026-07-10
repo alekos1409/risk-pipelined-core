c9009a3530f4ef2657b1e694fec4e295df30503e
alekos1409 <alekoskipouros448@gmail.com>
2026-07-10 09:43:40 +0300

    Update top_unit_pipelined_test.v

--- testbenches/top_unit_pipelined_test.v
+++ testbenches/top_unit_pipelined_test.v
@@ -1,9 +1,19 @@
 module top_unit_pipelined_test();
 reg reset,clk;
+wire tx,byte_valid;
+wire [7:0]received_byte;
 
-top_unit_pipelined uut(
+top_unit_pipelined uut1(
     .reset(reset),
-    .clk(clk)
+    .clk(clk),
+    .tx(tx)
+);
+UART_rx uut2(
+.clk(clk),
+.reset(reset),
+.tx(tx),
+.received_byte(received_byte),
+.byte_valid(byte_valid)
 );
 initial begin
     $dumpfile("top_unit_pipelined.vcd");
@@ -14,11 +24,15 @@ always
 begin clk = ~clk;
 #5;
 end 
+always @(posedge clk) begin
+    if (byte_valid)
+        $write("%c", received_byte);
+end
 initial begin
     reset<=1'b1;
     #40;
     reset<=1'b0;
-    #400;
+    #8000;
     $finish;
 end
 endmodule

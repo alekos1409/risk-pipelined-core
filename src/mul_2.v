1e47fd46772bf0d4bf0d94151e5bd047c36302cb
alekos1409 <alekoskipouros448@gmail.com>
2026-07-17 10:53:16 +0300

    mul_2

--- src/mul_2.v
+++ src/mul_2.v
@@ -15,36 +15,35 @@ module mul_2 (
   reg [63:0] a_in, b_in;
   reg [5:0] count = 32;
   reg done_in;
-   reg start_prev;
-  parameter idle = 2'b00, start_p = 2'b01 , done_p = 2'b10;
+  reg start_prev;
+  parameter idle = 2'b00, start_p = 2'b01, done_p = 2'b10;
   reg [1:0] state = idle;
-always @(posedge clk) begin
-     start_prev <= start;
+  always @(posedge clk) begin
+    start_prev <= start;
     if (reset) begin
       a_in <= 0;
       b_in <= 0;
       Result_in <= 0;
       count <= 32;
       done_in <= 0;
-      state<=idle;
-       start_prev<=0;
-    end
-    else begin 
- case(state)
-    idle: begin
-         
-        a_in <= a;
+      state <= idle;
+      start_prev <= 0;
+    end else begin
+      case (state)
+        idle: begin
+
+          a_in <= a;
           b_in <= b;
           Result_in <= 0;
           count <= 32;
           done_in <= 0;
           if (start && !start_prev) begin
-          state<= start_p;
-         
-    end
+            state <= start_p;
+
+          end
         end
-    start_p: begin
-        if (count != 6'b0) begin
+        start_p: begin
+          if (count != 6'b0) begin
             if (b_in[0] == 1'b1) begin
               Result_in <= Result_in + a_in;
               b_in <= b_in >> 1;
@@ -55,19 +54,19 @@ always @(posedge clk) begin
               a_in  <= a_in << 1;
               count <= count - 1;
             end
-        end
-            if(count == 6'b0) begin
+          end
+          if (count == 6'b0) begin
             done_in <= 0;
-            state<= done_p;
+            state   <= done_p;
           end
         end
-    done_p: begin
+        done_p: begin
           done_in <= 1;
-          state <=idle;
+          state   <= idle;
         end
-  endcase
+      endcase
     end
-end
+  end
   assign Result = Result_in;
   assign done   = done_in;
 endmodule
